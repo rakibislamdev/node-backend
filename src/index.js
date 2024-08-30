ea7607afc5ccc829bea423e9c11f550a3c51547d
auntar <auntar@gmail.com>
2024-08-30 12:17:25 +0600

    last update

--- src/index.js
+++ src/index.js
@@ -6,3 +6,14 @@ dotenv.config({
 
 // database connection
 connectDB();
+
+// express server
+import express from "express";
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// routes
+import userRoutes from "./routes/userRoutes.js";
+app.use("/api/users", userRoutes);
